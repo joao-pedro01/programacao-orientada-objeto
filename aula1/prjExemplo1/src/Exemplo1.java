cb5dfdc33204b06578d9d131f57fd8cf5fecacdf
joao-pedro01 <joao_pedro01@terra.com.br>
2023-02-14 11:11:48 -0300

    Add scanner and printf

---
+++ aula1/prjExemplo1/src/Exemplo1.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author joao-pedro01
+ */
+public class Exemplo1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int codProd, qntVend;
+        String descProd;
+        double preocUnit, valCompra;
+
+        System.out.print("Digite o código do produto: ");
+        
+    } // fim do main
+} // fim da classe
