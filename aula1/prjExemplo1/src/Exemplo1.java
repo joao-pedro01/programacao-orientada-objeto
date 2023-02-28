dfa5b498f5dee2469d8f0b4c0274a84258f5e2be
joao-pedro01 <joao_pedro01@terra.com.br>
2023-02-28 10:40:28 -0300

    End projeto aula1

--- aula1/prjExemplo1/src/Exemplo1.java
+++ aula1/prjExemplo1/src/Exemplo1.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 /**
@@ -10,9 +9,29 @@ public class Exemplo1 {
         Scanner entrada = new Scanner(System.in);
         int codProd, qntVend;
         String descProd;
-        double preocUnit, valCompra;
+        double precoUnit, valCompra, desconto;
 
         System.out.print("Digite o código do produto: ");
+        codProd = entrada.nextInt();//scanf("%d", &codProd)
         
+        System.out.println("Digite a descrição: ");
+        descProd = entrada.next();//scanf("%s", &descProd)
+        
+        System.out.println("Digite o preço unitário: ");
+        precoUnit = entrada.nextDouble();//scanf("%lf", &precoUnit)
+        
+        System.out.println("Digite a quantidade ventidida: ");
+        qntVend = entrada.nextInt();//scanf("%i", &qntVend)
+        
+        valCompra = precoUnit * qntVend;
+        
+        if (valCompra >= 850) {
+            desconto = 0.10 * valCompra;
+        }else {
+            desconto = 0;
+        }
+        valCompra -= desconto;
+        System.out.println("O valor do desconto é: " + desconto);
+        System.out.println("\nValor da compra: " + valCompra);
     } // fim do main
 } // fim da classe
