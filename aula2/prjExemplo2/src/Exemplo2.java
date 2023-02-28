5eca210d1b30eaef7aeab1ea5bc98e2c601c4a07
joao-pedro01 <joao_pedro01@terra.com.br>
2023-02-28 12:35:44 -0300

    Update aula 2

--- aula2/prjExemplo2/src/Exemplo2.java
+++ aula2/prjExemplo2/src/Exemplo2.java
@@ -1,13 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.Scanner;
 
 /**
  *
- * @author 1780482121031
+ * @author joao-pedro01
  */
 public class Exemplo2 {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int num, result;
+
+        System.out.printf("Digite o numero que deseja multiplicar: ");
+        num = entrada.nextInt();
+        
+        for (int i = 0; i <= 10; i++) {
+            result = num * i;
+            System.out.println(num + " X " + i + " = " + result);
+        }
+    }
 }
