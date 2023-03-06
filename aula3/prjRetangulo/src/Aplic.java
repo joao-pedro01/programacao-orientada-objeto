614d062168171a8881bce05260b69e8bd1f044c5
joao-pedro01 <joao_pedro01@terra.com.br>
2023-03-06 09:19:42 -0300

    Add obj Retangulo e atribuição retangulo

---
+++ aula3/prjRetangulo/src/Aplic.java
@@ -0,0 +1,26 @@
+import fatec.poo.model.Retangulo;
+import java.util.Scanner;
+
+/**
+ *
+ * @author joao-pedro01
+ */
+public class Aplic {
+    public static void main(String[] args) {
+        double medAlt, medBase;
+        Retangulo objRet = new Retangulo();
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Digite o valor da altura: ");
+        medAlt = entrada.nextDouble();
+        System.out.print("Digite o valor da base: ");
+        medBase = entrada.nextDouble();
+        
+        objRet.setAltura(medAlt);
+        objRet.setBase(medBase);
+        
+        System.out.println("\nMedida da altura: " + objRet.getAltura());
+        System.out.println("Medida de perimetro: " + objRet.calcPerimetro());
+        System.out.println("Medida da área: " + objRet.calcArear());
+    }   
+}
--- aula3/prjRetangulo/src/fatec/poo/model/Retangulo.java
+++ aula3/prjRetangulo/src/fatec/poo/model/Retangulo.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fatec.poo.model;
 
 /**
  *
- * @author 1780482121031
+ * @author joao-pedro01
  */
 public class Retangulo {
+    private double altura;
+    private double base;
     
+    public void setAltura(double a) {
+        altura = a;
+    }
+    public void setBase(double b) {
+        base = b;
+    }
+
+    public double getAltura() {
+        return(altura);
+    }
+    public double getBase() {
+        return(base);
+    }
+    
+    public double calcArear() {
+        return(altura * base);
+    }
+    public double calcPerimetro() {
+        return(2* (altura + base));
+    }
 }
